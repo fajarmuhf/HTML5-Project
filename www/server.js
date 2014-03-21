90bb2404e3797e68f70e693e04dfce59559d7088
Fajar Muhammad F <fajarmuhf@live.com>
2014-03-21 16:07:22 +0700

    project

--- www/server.js
+++ www/server.js
@@ -1,4 +1,10 @@
-var io = require('socket.io').listen(8080);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+var io = require('socket.io').listen(port);
 
 io.sockets.on('connection', function (socket) {
   console.log('emit...');
